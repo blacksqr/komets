27ade719c845aad945f7a1483186deaec97061ff
dhm.masson <dhm.masson@86749b28-2505-11de-8c12-6dbec8df7899>
2010-03-03 18:07:54 +0000

    ajout de la largeur et de la hauteur

--- models/HTML/model_PM_P_HTML.tcl
+++ models/HTML/model_PM_P_HTML.tcl
@@ -430,8 +430,22 @@ method PM_HTML Bg_gradient {color1 color2} {
 	lassign $color2 r1 g1 b1 a1 
 	this add_html_style [list "background" "-moz-linear-gradient(0deg, rgba([expr int(256 * $r)],[expr int(256 * $g)],[expr int(256 * $b)],$a), rgba([expr int(256 * $r1)],[expr int(256 * $g1)],[expr int(256 * $b1)],$a1))" ]
 }
+
+method PM_HTML HEIGHT {x} {
+	this add_html_style [list "height" "$x%"]
+}
+
+method PM_HTML WIDTH {x} {
+	this add_html_style [list "width" "$x%"]
+}
+method PM_HTML LEFT {x} {
+	this add_html_style [list "left" "$x%"]
+}
+
+method PM_HTML TOP {x} {
+	this add_html_style [list "top" "$x%"]
+}
 method PM_HTML FG {color1} {
 	lassign $color1 r g b a 
-
 	this add_html_style [list "color" "rgba([expr int(256 * $r)],[expr int(256 * $g)],[expr int(256 * $b)],$a)"]
 }
